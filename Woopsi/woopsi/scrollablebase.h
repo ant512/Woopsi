015d6119d728a1da38598e83eec1c6bd930ca3b5
ant512 <devnull@localhost>
2008-03-10 14:43:56 +0000

    - Added javadoc comments to scrollablebase.h.

--- Woopsi/woopsi/scrollablebase.h
+++ Woopsi/woopsi/scrollablebase.h
@@ -3,18 +3,59 @@
 
 #include <nds.h>
 
+/**
+ * Abstract class providing some basic functionality required by scrolling gadgets.
+ * Scrolling regions are modelled as a virtual "canvas", or rectangular region, with
+ * height/width dimensions and x/y co-ordinates.
+ */
 class ScrollableBase {
 public:
+	/**
+	 * Constructor.
+	 */
 	ScrollableBase() { }
+	
+	/**
+	 * Destructor.
+	 */
 	virtual ~ScrollableBase() { }
 
+	/**
+	 * Gets the x co-ordinate of the virtual canvas.
+	 * @return The x co-ordinate of the virtual canvas.
+	 */
 	virtual const s32 getCanvasX() { return _canvasX; };
+	
+	/**
+	 * Gets the y co-ordinate of the virtual canvas.
+	 * @return The y co-ordinate of the virtual canvas.
+	 */
 	virtual const s32 getCanvasY() { return _canvasY; };
 
+	/**
+	 * Gets the width of the virtual canvas.
+	 * @return The width of the virtual canvas.
+	 */
 	virtual const s32 getCanvasWidth() { return _canvasWidth; };
+	
+	/**
+	 * Gets the height of the virtual canvas.
+	 * @return The height of the virtual canvas.
+	 */
 	virtual const s32 getCanvasHeight() { return _canvasHeight; };
 
+	/**
+	 * Scrolls the virtual canvas by the specified amounts.
+	 * @param dx Distance to scroll horizontally.
+	 * @param dy Distance to scroll vertically.
+	 */
 	virtual void scroll(s32 dx, s32 dy) = 0;
+	
+	/**
+	 * Repositions the virtual canvas to the specified co-ordinates.
+	 * @param x New x co-ordinate of the virtual canvas.
+	 * @param y New y co-ordinate of the virtual canvas.
+	 */
 	virtual void jump(s32 x, s32 y) = 0;
 
 protected:
