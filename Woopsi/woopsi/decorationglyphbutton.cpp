1eb0bfca2aae2ac665deda3c7f7caa84de6dc348
ant512 <devnull@localhost>
2008-03-17 15:31:15 +0000

    - Added DecorationGlyphButton class.

---
+++ Woopsi/woopsi/decorationglyphbutton.cpp
@@ -0,0 +1,57 @@
+#include "decorationglyphbutton.h"
+
+DecorationGlyphButton::DecorationGlyphButton(s16 x, s16 y, u16 width, u16 height, char normalGlyph, char clickedGlyph, FontBase* font) : Button(x, y, width, height, normalGlyph, font) {
+	_flags.decoration = true;
+
+	_normalGlyph = normalGlyph;
+	_clickedGlyph = clickedGlyph;
+}
+
+DecorationGlyphButton::~DecorationGlyphButton() {
+}
+
+void DecorationGlyphButton::draw() {
+	Gadget::draw();
+}
+
+void DecorationGlyphButton::draw(Rect clipRect) {
+
+	GraphicsPort* port = newInternalGraphicsPort(clipRect);
+
+	// Clear the background
+	port->drawFilledRect(0, 0, _width, _height, _backColour);
+
+	// Draw the glyph
+	if (_flags.clicked) {
+		port->drawText(_textX, _textY, _font, _clickedGlyph);
+	} else {
+		port->drawText(_textX, _textY, _font, _normalGlyph);
+	}
+
+	// Draw outline
+	port->drawBevelledRect(0, 0, _width, _height);
+	
+	delete port;
+}
+
+bool DecorationGlyphButton::release(s16 x, s16 y) {
+
+	if (Gadget::release(x, y)) {
+		Gadget::draw();
+		return true;
+	}
+
+	return false;
+}
+
+bool DecorationGlyphButton::focus() {
+	if (!_flags.active) {
+		_flags.active = true;
+
+		Gadget::draw();
+
+		return true;
+	}
+
+	return false;
+}
---
+++ Woopsi/woopsi/decorationglyphbutton.h
@@ -0,0 +1,30 @@
+#ifndef _DECORATION_GLYPH_BUTTON_H_
+#define _DECORATION_GLYPH_BUTTON_H_
+
+#include <nds.h>
+#include "button.h"
+
+using namespace std;
+
+/**
+ * Class representing a button that will display a glyph and form part
+ * of another gadget's border decorations.  Examples include the
+ * screen flip/depth buttons.
+ */
+class DecorationGlyphButton : public Button {
+
+public:
+	DecorationGlyphButton(s16 x, s16 y, u16 width, u16 height, char normalGlyph, char clickedGlyph, FontBase* font = NULL);
+	~DecorationGlyphButton();
+
+	virtual void draw();
+	virtual void draw(Rect clipRect);
+	virtual bool focus();
+	virtual bool release(s16 x, s16 y);
+
+protected:
+	char _normalGlyph;
+	char _clickedGlyph;
+};
+
+#endif
