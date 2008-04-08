bbc425f3c4ff1a729778392a956af253aa4a13b9
ant512 <devnull@localhost>
2008-04-08 14:58:47 +0000

    - More javadoc comments.

--- Woopsi/woopsi/button.h
+++ Woopsi/woopsi/button.h
@@ -11,7 +11,27 @@ using namespace std;
  */
 class Button : public Textbox {
 public:
+
+	/**
+	 * Constructor for buttons that display a string.
+	 * @param x The x co-ordinate of the button, relative to its parent.
+	 * @param y The y co-ordinate of the button, relative to its parent.
+	 * @param width The width of the button.
+	 * @param height The height of the button.
+	 * @param text The text for the button to display.
+	 * @param font The font that the button should use.
+	 */
 	Button(s16 x, s16 y, u16 width, u16 height, char* text, FontBase* font = NULL);
+
+	/**
+	 * Constructor for buttons that display a single char.
+	 * @param x The x co-ordinate of the button, relative to its parent.
+	 * @param y The y co-ordinate of the button, relative to its parent.
+	 * @param width The width of the button.
+	 * @param height The height of the button.
+	 * @param letter The character for the button to display.
+	 * @param font The font that the button should use.
+	 */
 	Button(s16 x, s16 y, u16 width, u16 height, char letter, FontBase* font = NULL);
 
 protected:
