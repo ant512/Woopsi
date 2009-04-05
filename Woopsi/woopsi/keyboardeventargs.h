076af573143aa851de27ce705388210ade965fe8
ant512 <devnull@localhost>
2009-04-05 16:49:50 +0000

    - Added missing keyboard event files.

---
+++ Woopsi/woopsi/keyboardeventargs.h
@@ -0,0 +1,36 @@
+#include "eventargs.h"
+
+#ifndef _KEYBOARDEVENTARGS_H_
+#define _KEYBOARDEVENTARGS_H_
+
+namespace WoopsiUI {
+
+	class WoopsiKey;
+	class WoopsiKeyboard;
+
+	/**
+	 * Event arguments passed to listeners when a WoopsiKeyboard object raises an event.
+	 */
+	class KeyboardEventArgs : public EventArgs<WoopsiKeyboard*> {
+	public:
+
+		/**
+		 * Constructor.
+		 * @param source Pointer to the WoopsiKeyboard object that raised the event.
+		 */
+		KeyboardEventArgs(WoopsiKeyboard* source, WoopsiKey* key) : EventArgs<WoopsiKeyboard*>(source) {
+			_key = key;
+		};
+
+		/**
+		 * Get the key that caused the event.
+		 * @return Pointer to the key that caused the event.
+		 */
+		inline const WoopsiKey* getKey() const { return _key; };
+
+	private:
+		WoopsiKey* _key;
+	};
+}
+
+#endif
---
+++ Woopsi/woopsi/keyboardeventhandler.h
@@ -0,0 +1,47 @@
+#ifndef _KEYBOARDEVENTHANDLER_H_
+#define _KEYBOARDEVENTHANDLER_H_
+
+#include <nds.h>
+#include "keyboardeventargs.h"
+
+namespace WoopsiUI {
+
+	class Gadget;
+
+	/**
+	 * Base keyboard event handler, intended to be subclasses.  The WoopsiKeyboard class
+	 * raises events to instances of this class for processing.
+	 */
+	class KeyboardEventHandler {
+	public:
+		/**
+		 * Constructor.
+		 */
+		inline KeyboardEventHandler() { }
+		
+		/**
+		 * Destructor.
+		 */
+		virtual inline ~KeyboardEventHandler() { }
+		
+		/**
+		 * Handle a keyboard press event.
+		 * @param e The event data.
+		 */
+		virtual void handleKeyboardPressEvent(const KeyboardEventArgs& e) { };
+
+		/**
+		 * Handle a keyboard repeat event.
+		 * @param e The event data.
+		 */
+		virtual void handleKeyboardRepeatEvent(const KeyboardEventArgs& e) { };
+
+		/**
+		 * Handle a keyboard release event.
+		 * @param e The event data.
+		 */
+		virtual void handleKeyboardReleaseEvent(const KeyboardEventArgs& e) { };
+	};
+}
+
+#endif
