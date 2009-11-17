bc54dac820b3558ade57be0a4733a5025bba57ec
ant512 <devnull@localhost>
2009-11-17 16:04:58 +0000

    - Added DOS batch files to build examples and tests.

---
+++ Woopsi/examples/build.bat
@@ -0,0 +1,82 @@
+cd bitmapdrawing
+make clean
+make
+
+cd ..
+cd bitmaploader
+make clean
+make
+
+cd ..
+cd calendar
+make clean
+make
+
+cd ..
+cd contextmenu
+make clean
+make
+
+cd ..
+cd date
+make clean
+make
+
+cd ..
+cd dimmedscreen
+make clean
+make
+
+cd ..
+cd filerequester
+make clean
+make
+
+cd ..
+cd gadgetdrawing
+make clean
+make
+
+cd ..
+cd helloworld
+make clean
+make
+
+cd ..
+cd helloworld_alternative
+make clean
+make
+
+cd ..
+cd keyboard
+make clean
+make
+
+cd ..
+cd progressbar
+make clean
+make
+
+cd ..
+cd scrolltest
+make clean
+make
+
+cd ..
+cd skins
+make clean
+make
+
+cd ..
+cd timer
+make clean
+make
+
+cd ..
+cd wpaint
+make clean
+make
+
+cd ..
+
+pause
---
+++ Woopsi/tests/build.bat
@@ -0,0 +1,36 @@
+cd button
+make clean
+make
+
+cd ..
+cd calendar
+make clean
+make
+
+cd ..
+cd cyclebutton
+make clean
+make
+
+cd ..
+cd label
+make clean
+make
+
+cd ..
+cd multilinetextbox
+make clean
+make
+
+cd ..
+cd radiobuttongroup
+make clean
+make
+
+cd ..
+cd textbox
+make clean
+make
+
+cd ..
+pause
