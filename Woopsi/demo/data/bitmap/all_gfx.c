de3f927cdcd353db84dbaac678b8e7f3763b3bf8
ant512 <devnull@localhost>
2009-11-19 15:15:11 +0000

    - Moved IK bitmaps out of demo folder/NDS into animbutton test.

--- Woopsi/demo/data/bitmap/all_gfx.c
+++ Woopsi/demo/data/bitmap/all_gfx.c
@@ -11,16 +11,6 @@ extern "C" {
 #include "bittest4.c"
 #include "bittest5.c"
 #include "bittest6.c"
-#include "ik1.c"
-#include "ik2.c"
-#include "ik3.c"
-#include "ik4.c"
-#include "ik5.c"
-#include "ik6.c"
-#include "ik7.c"
-#include "ik8.c"
-#include "ik9.c"
-#include "ik10.c"
 
 #ifdef __cplusplus
 }
--- Woopsi/demo/data/bitmap/all_gfx.h
+++ Woopsi/demo/data/bitmap/all_gfx.h
@@ -8,16 +8,6 @@ extern const unsigned short bittest3_Bitmap[2576] __attribute__ ((aligned (4))) 
 extern const unsigned short bittest4_Bitmap[2576] __attribute__ ((aligned (4))) ;  // 16bit bitmap
 extern const unsigned short bittest5_Bitmap[2576] __attribute__ ((aligned (4))) ;  // 16bit bitmap
 extern const unsigned short bittest6_Bitmap[2576] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik1_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik2_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik3_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik4_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik5_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik6_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik7_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik8_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik9_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
-extern const unsigned short ik10_Bitmap[5883] __attribute__ ((aligned (4))) ;  // 16bit bitmap
 
 #endif
 
