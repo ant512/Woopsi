289b7f1931ae62539327fe2bf07168415e9aedc4
ant512 <devnull@localhost>
2010-01-13 13:23:09 +0000

    - Fixed memory leak when enumerating directories in FileListBox.  - FileListBox shows dummy file list when in SDL mode.

--- Woopsi/libwoopsi/src/filelistbox.cpp
+++ Woopsi/libwoopsi/src/filelistbox.cpp
@@ -1,10 +1,15 @@
 #include <nds.h>
-#include <fcntl.h>
-#include <dirent.h>
 #include "filelistbox.h"
 #include "button.h"
 #include "filepath.h"
 #include "graphicsport.h"
+
+#ifndef USING_SDL
+
+#include <fcntl.h>
+#include <dirent.h>
+
+#endif
 
 using namespace WoopsiUI;
 
@@ -91,15 +96,57 @@ void FileListBox::readDirectory() {
 	
 	// Remove the "Loading..." option
 	_listbox->removeAllOptions();
-	
+
+#ifdef USING_SDL
+
+	// Build file list using SDL
+	// We build a dummy list.  Most OSes use different
+	// methods for accessing the file system.  We could
+	// target POSIX, but then we cut out Windows; or we
+	// could target Windows and POSIX, but then we cut
+	// out any number of other OSes.  Simplest solution
+	// is just to present a list of imaginary files.
+
+	// Which path are we building?
+	if (_path->getPath().compareTo("/") == 0) {
+
+		// Root path
+
+		// Directories
+		_listbox->addOption(new FileListBoxDataItem("Directory1", 0, getShineColour(), getBackColour(), getShineColour(), getHighlightColour(), true));
+		_listbox->addOption(new FileListBoxDataItem("Directory2", 0, getShineColour(), getBackColour(), getShineColour(), getHighlightColour(), true));
+		_listbox->addOption(new FileListBoxDataItem("Directory3", 0, getShineColour(), getBackColour(), getShineColour(), getHighlightColour(), true));
+		_listbox->addOption(new FileListBoxDataItem("Directory4", 0, getShineColour(), getBackColour(), getShineColour(), getHighlightColour(), true));
+
+		// Files
+		_listbox->addOption(new FileListBoxDataItem("File1", 0, getShadowColour(), getBackColour(), getShadowColour(), getHighlightColour(), false));
+		_listbox->addOption(new FileListBoxDataItem("File2", 0, getShadowColour(), getBackColour(), getShadowColour(), getHighlightColour(), false));
+		_listbox->addOption(new FileListBoxDataItem("File3", 0, getShadowColour(), getBackColour(), getShadowColour(), getHighlightColour(), false));
+		_listbox->addOption(new FileListBoxDataItem("File4", 0, getShadowColour(), getBackColour(), getShadowColour(), getHighlightColour(), false));
+	} else if (_path->getPath().compareTo("/Directory1/") == 0) {
+
+		// Directory 1
+
+		// Directories
+		_listbox->addOption(new FileListBoxDataItem("Subdir1", 0, getShineColour(), getBackColour(), getShineColour(), getHighlightColour(), true));
+		_listbox->addOption(new FileListBoxDataItem("Subdir2", 0, getShineColour(), getBackColour(), getShineColour(), getHighlightColour(), true));
+
+		// Files
+		_listbox->addOption(new FileListBoxDataItem("Subfile1", 0, getShadowColour(), getBackColour(), getShadowColour(), getHighlightColour(), false));
+	}
+
+#else
+
+	// Build file list using libfat
 	struct stat st;
 
+	// Get a copy of the path char array so that it can be used with libfat
 	char* path = new char[_path->getPath().getLength() + 1];
 	_path->getPath().copyToCharArray(path);
 
 	DIR* dir = opendir(path);
 
-	delete[] path;
+	delete [] path;
 
 	// Did we get the dir successfully?
 	if (dir == NULL) return;
@@ -120,6 +167,8 @@ void FileListBox::readDirectory() {
 		}
 
 		// Create memory to store the filename in the array
+		// TODO: Can we just pass ent->d_name to the FileListBoxDataItem
+		// constructor, and let the WoopsiString handle the copying?
 		char* storedFilename = new char[strlen(ent->d_name) + 1];
 		strcpy(storedFilename, ent->d_name);
 
@@ -133,7 +182,11 @@ void FileListBox::readDirectory() {
 			// File
 			_listbox->addOption(new FileListBoxDataItem(storedFilename, 0, getShadowColour(), getBackColour(), getShadowColour(), getHighlightColour(), false));
 		}
+
+		delete storedFilename;
 	}
+
+#endif
 	
 	// Re-enable drawing now that the list is complete
 	if (drawingEnabled) {
