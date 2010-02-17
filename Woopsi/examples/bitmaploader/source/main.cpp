ba059138cf7b772cca4cf270d3dc1b520538a7aa
ant512 <devnull@localhost>
2010-02-17 18:08:10 +0000

    - More branch merging.

--- Woopsi/examples/bitmaploader/source/main.cpp
+++
@@ -1,9 +0,0 @@
-// Includes
-#include "bitmaploader.h"
-
-int main(int argc, char* argv[]) {
-
-	// Create the bitmap loader application
-	BitmapLoader app;
-	return app.main(argc, argv);
-}
--- Woopsi/examples/skins/source/main.cpp
+++
@@ -1,9 +0,0 @@
-// Includes
-#include "skins.h"
-
-int main(int argc, char* argv[]) {
-
-	// Create the Hello World application
-	Skins app;
-	return app.main(argc, argv);
-}
