cebf485c63464189ce3230c543bfc1b4ed136ea3
ant512 <devnull@localhost>
2010-07-28 17:08:19 +0000

    - PackedFont1 clips correctly.

--- Woopsi/libwoopsi/src/packedfont1.cpp
+++ Woopsi/libwoopsi/src/packedfont1.cpp
@@ -9,6 +9,12 @@ void PackedFont1::renderChar(
 		s16 x, s16 y,
 		u16 clipX1, u16 clipY1, u16 clipX2, u16 clipY2)
 {
+	// Abort if there is nothing to render
+	if ((clipY2 < y) ||
+		(clipY1 > y + getHeight() - 1) ||
+		(x > clipX2) ||
+		(x + pixelsPerRow < clipX1)) return;
+	
 	u16 colour = getColour();
 	u16 curr;
 	u16 mask;
