70634475a759d00cd9f286a5a123109ee861d684
ant512 <devnull@localhost>
2010-09-03 13:29:48 +0100

    Tests build script uses for loop instead of explicitly named folders.

--- Woopsi/tests/build.bat
+++ Woopsi/tests/build.bat
@@ -1,81 +1,10 @@
 @echo off
 
-cd alert
-make clean
-make
+for /d %%x in (.\*) do (
+	cd %%x
+	make clean
+	make
+	cd ..
+)
 
-cd ../animbutton
-make clean
-make
-
-cd ../bitmapbutton
-make clean
-make
-
-cd ../button
-make clean
-make
-
-cd ../calendar
-make clean
-make
-
-cd ../colourpicker
-make clean
-make
-
-cd ../cyclebutton
-make clean
-make
-
-cd ../filerequester
-make clean
-make
-
-cd ../label
-make clean
-make
-
-cd ../listbox
-make clean
-make
-
-cd ../multilinetextbox
-make clean
-make
-
-cd ../progressbar
-make clean
-make
-
-cd ../radiobuttongroup
-make clean
-make
-
-cd ../requester
-make clean
-make
-
-cd ../scrollbarhorizontal
-make clean
-make
-
-cd ../scrollbarvertical
-make clean
-make
-
-cd ../scrollinglistbox
-make clean
-make
-
-cd ../sliderhorizontal
-make clean
-make
-
-cd ../slidervertical
-make clean
-make
-
-cd ../textbox
-make clean
-make
+pause
