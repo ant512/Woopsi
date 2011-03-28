aac1f126a0f3defe89fdb7be65bac66e496d6446
ant512 <devnull@localhost>
2011-03-28 14:57:10 +0100

    Added MakeRelease batch file.

---
+++ MakeRelease.bat
@@ -0,0 +1,23 @@
+@echo off
+
+call hg archive -tfiles C:\Woopsi
+
+rem Tidy up directory structure
+cd C:\Woopsi\
+del .hg_archival.txt
+del .hgignore
+del .hgtags
+del Woopsi.sln
+del ToDo.txt
+rmdir Woopsi\images /s /q
+rmdir Woopsi\Release /s /q
+del Woopsi\Woopsi.vcproj
+move README.txt Woopsi\
+
+rem Build library
+cd Woopsi\libwoopsi
+make
+rmdir build /s /q
+
+rem Finishing touch
+del MakeRelease.bat
