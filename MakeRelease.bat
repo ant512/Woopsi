c45fef32c60deebf388f9ed43a513073803600a0
ant512 <devnull@localhost>
2011-04-06 15:54:14 +0100

    MakeRelease creates documentation automatically.

--- MakeRelease.bat
+++ MakeRelease.bat
@@ -13,4 +13,12 @@ rmdir Woopsi\images /s /q
 rmdir Woopsi\Release /s /q
 del Woopsi\Woopsi.vcproj
 move README.txt Woopsi\
+
+rem Create documentation
+mkdir Woopsi\documentation
+"C:\Program Files (x86)\doxygen\bin\doxygen" C:\Woopsi\Doxyfile
+move Woopsi\documentation\html\** Woopsi\documentation
+rmdir Woopsi\documentation\html
+del Doxyfile
+
 del MakeRelease.bat
