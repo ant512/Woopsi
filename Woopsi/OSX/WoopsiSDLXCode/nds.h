f5a74665b04ee346fb9a162611d7c6a20f74d5e2
ant512 <ant@simianzombie.com>
2014-01-01 20:38:17 -0800

    Merged in Xcode project file.

---
+++ Woopsi/OSX/WoopsiSDLXCode/nds.h
@@ -0,0 +1,24 @@
+#ifndef _NDS_H_
+#define _NDS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+
+#define USING_SDL
+
+#define u16 Uint16
+#define s16 Sint16
+#define u8 Uint8
+#define s8 Sint8
+#define u32 Uint32
+#define s32 Sint32
+
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 192
+
+int fatInitDefault();
+
+#endif
