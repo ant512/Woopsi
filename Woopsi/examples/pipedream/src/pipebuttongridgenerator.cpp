2d275864328c4f89d7ddda486e9bfd8212106a2f
ant512 <ant@simianzombie.com>
2014-01-26 17:59:06 -0800

    Deleted pathfinder.

--- Woopsi/examples/pipedream/src/pipebuttongridgenerator.cpp
+++ Woopsi/examples/pipedream/src/pipebuttongridgenerator.cpp
@@ -39,78 +39,69 @@ void PipeButtonGridGenerator::placeTileAtRandomCoordinates(TileType tile, u8* gr
 u8* PipeButtonGridGenerator::generateRandomTiles(u8 blockingTileCount) {
 	u8* layout = new u8[_rows * _columns];
 
-	// We keep generating new layouts until we create one that can be solved.
-	bool solvable = false;
+	// Erase the grid to begins
+	for (u8 i = 0; i < _rows; ++i) {
+		fillRow(i, TILE_TYPE_BLANK, layout);
+	}
 
-	do {
+	// Fill top and bottom with blocking tiles
+	fillColumn(0, TILE_TYPE_BLOCKING, layout);
+	fillColumn(_columns - 1, TILE_TYPE_BLOCKING, layout);
 
-		// Erase the grid to begins
-		for (u8 i = 0; i < _rows; ++i) {
-			fillRow(i, TILE_TYPE_BLANK, layout);
+	// Fill left and right with blocking tiles
+	fillRow(0, TILE_TYPE_BLOCKING, layout);
+	fillRow(_rows - 1, TILE_TYPE_BLOCKING, layout);
+
+	// Place start and end tiles
+	bool startAtTop = rand() % 2;
+
+	if (startAtTop) {
+		_start.setY(0);
+		_end.setY(_rows - 1);
+
+		if (_columns - 2 > 0) {
+			_start.setX(1 + (rand() % (_columns - 2)));
 		}
 
-		// Fill top and bottom with blocking tiles
-		fillColumn(0, TILE_TYPE_BLOCKING, layout);
-		fillColumn(_columns - 1, TILE_TYPE_BLOCKING, layout);
-
-		// Fill left and right with blocking tiles
-		fillRow(0, TILE_TYPE_BLOCKING, layout);
-		fillRow(_rows - 1, TILE_TYPE_BLOCKING, layout);
-
-		// Place start and end tiles
-		bool startAtTop = rand() % 2;
-
-		if (startAtTop) {
-			_start.setY(0);
-			_end.setY(_rows - 1);
-
-			if (_columns - 2 > 0) {
-				_start.setX(1 + (rand() % (_columns - 2)));
-			}
-
-			// We want to ensure that the start and end are not exactly opposite
-			do {
-				_end.setX(1 + (rand() % (_rows - 2)));
-			} while (_end.getX() == _start.getX());
-		} else {
-			_start.setX(0);
-			_end.setX(_columns - 1);
-			
-			if (_rows - 2 > 0) {
-				_start.setY(1 + (rand() % (_rows - 2)));
-			}
-
-			do {
-				_end.setY(1 + (rand() % (_rows - 2)));
-			} while (_end.getY() == _start.getY());
+		// We want to ensure that the start and end are not exactly opposite
+		do {
+			_end.setX(1 + (rand() % (_rows - 2)));
+		} while (_end.getX() == _start.getX());
+		
+		// Ensure that the start isn't blocked off by placing a random tile
+		// below it
+		layout[_start.getX() + ((_start.getY() + 1) * _columns)] = TILE_TYPE_VERTICAL + (rand() % 6);
+	} else {
+		_start.setX(0);
+		_end.setX(_columns - 1);
+		
+		if (_rows - 2 > 0) {
+			_start.setY(1 + (rand() % (_rows - 2)));
 		}
 
-		layout[_start.getX() + (_start.getY() * _columns)] = startAtTop ? TILE_TYPE_VERTICAL : TILE_TYPE_HORIZONTAL;
-		layout[_end.getX() + (_end.getY() * _columns)] = startAtTop ? TILE_TYPE_VERTICAL : TILE_TYPE_HORIZONTAL;
+		do {
+			_end.setY(1 + (rand() % (_rows - 2)));
+		} while (_end.getY() == _start.getY());
+		
+		// Ensure that the start isn't blocked off by placing a random tile next
+		// to it
+		layout[_start.getX() + 1 + (_start.getY() * _columns)] = TILE_TYPE_VERTICAL + (rand() % 6);
+	}
 
-		// We don't want more than 1/3 of the grid to be blocking tiles, as
-		// there's a good chance we can't create valid levels with more blocks
-		// (the ratio is chosen arbitrarily based on gut feel rather than
-		// testing).
-		if (blockingTileCount > ((_rows - 2) * (_columns - 2)) / 3) {
-			blockingTileCount = ((_rows - 2) * (_columns - 2)) / 3;
-		}
+	layout[_start.getX() + (_start.getY() * _columns)] = startAtTop ? TILE_TYPE_VERTICAL : TILE_TYPE_HORIZONTAL;
+	layout[_end.getX() + (_end.getY() * _columns)] = startAtTop ? TILE_TYPE_VERTICAL : TILE_TYPE_HORIZONTAL;
 
-		for (u8 i = 0; i < blockingTileCount; ++i) {
-			placeTileAtRandomCoordinates(TILE_TYPE_BLOCKING, layout);
-		}
+	// We don't want more than 1/3 of the grid to be blocking tiles, as
+	// there's a good chance we can't create valid levels with more blocks
+	// (the ratio is chosen arbitrarily based on gut feel rather than
+	// testing).
+	if (blockingTileCount > ((_rows - 2) * (_columns - 2)) / 3) {
+		blockingTileCount = ((_rows - 2) * (_columns - 2)) / 3;
+	}
 
-		// Insert the tiles required to solve the grid
-		bool* checked = new bool[_columns * _rows];
-
-		if (startAtTop) {
-			solvable = solveGrid(_start.getX(), _start.getY(), _start.getX(), _start.getY() + 1, _end.getX(), _end.getY(), layout, checked);
-		} else {
-			solvable = solveGrid(_start.getX(), _start.getY(), _start.getX() + 1, _start.getY(), _end.getX(), _end.getY(), layout, checked);
-		}
-
-		delete[] checked;
-	} while (!solvable);
+	for (u8 i = 0; i < blockingTileCount; ++i) {
+		placeTileAtRandomCoordinates(TILE_TYPE_BLOCKING, layout);
+	}
 
 	// Populate the empty space with random tiles.
 	for (u8 y = 1; y < _rows - 1; ++y) {
@@ -147,135 +138,3 @@ u8* PipeButtonGridGenerator::generateRandomTiles(u8 blockingTileCount) {
 
 	return layout;
 }
-
-bool PipeButtonGridGenerator::solveGrid(u8 previousX,
-										u8 previousY,
-										u8 currentX,
-										u8 currentY,
-										u8 endX,
-										u8 endY,
-										u8* grid,
-										bool* checked) {
-
-	// TODO: This needs to be rewritten so that, instead of favouring left, then
-	// right, then up, then down, it tries to move towards the exit first before
-	// falling back on the other directions.  Easiest way to achieve this is to
-	// populate a 4 element array with the desired order of directions, then
-	// iterate over the array trying each direction in preferred order.  That
-	// will make the algorithm always hunt the exit instead of (as is the case
-	// here) hug the walls or do other stupid stuff like pointless backtracking.
-	//
-	// Actually, having played it for a little while, this is sufficient...
-
-	u8 index = currentX + (_columns * currentY);
-
-	if (checked[index]) return false;
-
-	checked[index] = true;
-
-	if (currentX == endX && currentY == endY) {
-
-		// Found the exit!  Path complete
-		return true;
-	} else if (grid[index] != TILE_TYPE_BLANK) {
-
-		// Ran into a tile that's already used
-		return false;
-	} else if (currentX > 0 && (solveGrid(currentX, currentY, currentX - 1, currentY, endX, endY, grid, checked))) {
-
-		// Left
-		grid[index] = tileToConnectCoordinates(previousX, previousY, currentX, currentY, currentX - 1, currentY);
-		return true;
-	} else if (currentX < _columns - 1 && solveGrid(currentX, currentY, currentX + 1, currentY, endX, endY, grid, checked)) {
-
-		// Right
-		grid[index] = tileToConnectCoordinates(previousX, previousY, currentX, currentY, currentX + 1, currentY);
-		return true;
-	} else if (currentY > 0 && solveGrid(currentX, currentY, currentX, currentY - 1, endX, endY, grid, checked)) {
-
-		// Up
-		grid[index] = tileToConnectCoordinates(previousX, previousY, currentX, currentY, currentX, currentY - 1);
-		return true;
-	} else if (currentY < _rows - 1 && solveGrid(currentX, currentY, currentX, currentY + 1, endX, endY, grid, checked)) {
-
-		// Down
-		grid[index] = tileToConnectCoordinates(previousX, previousY, currentX, currentY, currentX, currentY + 1);
-		return true;
-	}
-
-	// No way through for these co-ordinates
-	return false;
-}
-
-PipeButtonGridGenerator::TileType PipeButtonGridGenerator::tileToConnectCoordinates(u8 previousX, u8 previousY, u8 currentX, u8 currentY, u8 nextX, u8 nextY) {
-	if (previousX == nextX) return TILE_TYPE_VERTICAL;
-	if (previousY == nextY) return TILE_TYPE_HORIZONTAL;
-
-	if (previousY > nextY && previousX < nextX) {
-		if (currentX == previousX) {
-			return TILE_TYPE_BOTTOM_RIGHT;
-		} else {
-			return TILE_TYPE_TOP_LEFT;
-		}
-	}
-
-	if (nextY > previousY && nextX < previousX) {
-		if (currentX == nextX) {
-			return TILE_TYPE_BOTTOM_RIGHT;
-		} else {
-			return TILE_TYPE_TOP_LEFT;
-		}
-	}
-
-	if (previousY < nextY && previousX < nextX) {
-		if (currentX == previousX) {
-			return TILE_TYPE_TOP_RIGHT;
-		} else {
-			return TILE_TYPE_BOTTOM_LEFT;
-		}
-	}
-
-	if (nextY < previousY && nextX < previousX) {
-		if (currentX == nextX) {
-			return TILE_TYPE_TOP_RIGHT;
-		} else {
-			return TILE_TYPE_BOTTOM_LEFT;
-		}
-	}
-
-	if (previousY > nextY && previousX > nextX) {
-		if (currentX == previousX) {
-			return TILE_TYPE_BOTTOM_LEFT;
-		} else {
-			return TILE_TYPE_TOP_RIGHT;
-		}
-	}
-
-	if (nextY > previousY && nextX > previousX) {
-		if (currentX == nextX) {
-			return TILE_TYPE_BOTTOM_LEFT;
-		} else {
-			return TILE_TYPE_TOP_RIGHT;
-		}
-	}
-
-	if (previousY < nextY && previousX > nextX) {
-		if (currentX == previousX) {
-			return TILE_TYPE_TOP_LEFT;
-		} else {
-			return TILE_TYPE_BOTTOM_RIGHT;
-		}
-	}
-
-	if (nextY < previousY && nextX > previousX) {
-		if (currentX == nextX) {
-			return TILE_TYPE_TOP_LEFT;
-		} else {
-			return TILE_TYPE_BOTTOM_RIGHT;
-		}
-	}
-
-	// No connection could be made!
-	return TILE_TYPE_BLANK;
-}
-
