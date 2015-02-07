66dcb653ca20d1a64f09f597350daf63fba440ff
ant512 <ant@simianzombie.com>
2015-02-06 22:10:33 -0800

    More DMA tidying.

--- Woopsi/libwoopsi/src/dmafuncs.cpp
+++ Woopsi/libwoopsi/src/dmafuncs.cpp
@@ -1,90 +1,93 @@
 #include <nds.h>
 #include "dmafuncs.h"
 
-#ifdef USING_SDL
+#ifndef USING_SDL
+
+static const u32 MEM_VRAM_START = 0x06000000;
+static const u32 MEM_VRAM_END = 0x06400000;
+
+#endif
 
 void woopsiDmaCopy(const u16* source, u16* dest, u32 count) {
-	memcpy(dest, source, sizeof(u16) * count);
-}
 
-void woopsiDmaFill(u16 fill, u16* dest, u32 count) {
-	for (u32 i = 0; i < count; i++) {
-		*(dest + i) = fill;
-	}
-}
+#ifdef USING_SDL
+
+    memcpy(dest, source, sizeof(u16) * count);
 
 #else
 
-void woopsiDmaCopy(const u16* source, u16* dest, u32 count) {
+    // Get memory addresses of source and destination
+    u32 srca = (u32)source;
+    u32 dsta = (u32)dest;
 
-	// Get memory addresses of source and destination
-	u32 srca = (u32)source;
-	u32 dsta = (u32)dest;
+    // Use DMA hardware if both source and destination are within VRAM
+    if (0 && (dsta >= MEM_VRAM_START) && (dsta < MEM_VRAM_END) && (srca >= MEM_VRAM_START) && (srca < MEM_VRAM_END)) {
 
-	// Precalculate boundaries of framebuffer VRAM
-	u32 bmp[2];
-	bmp[0] = 0x06000000;
-	bmp[1] = 0x06400000;
+        // libnds DMA functions work in bytes
+        count *= 2;
 
-	// Use DMA hardware if both source and destination are within VRAM
-    if ((dsta >= bmp[0]) && (dsta < bmp[1]) && (srca >= bmp[0]) && (srca < bmp[1])) {
+        DC_FlushRange(source, count);
 
-		// libnds DMA functions work in bytes
-		count *= 2;
+        while (dmaBusy(3)) {}
 
-		DC_FlushRange(source, count);
+        // Choose fastest DMA copy mode
+        if ((srca | dsta | count) & 3) {
+            dmaCopyHalfWordsAsynch(3, source, dest, count);
+        } else {
+            dmaCopyWordsAsynch(3, source, dest, count);
+        }
+    } else {
+        // Cannot use DMA as not working exclusively with VRAM
+		for (u32 i = 0; i < count; i++) {
+			*(dest + i) = *(source + i);
+		}
+    }
 
-		// Choose fastest DMA copy mode
-		if((srca|dsta|count) & 3)
-			dmaCopyHalfWordsAsynch(3, source, dest, count);
-		else
-			dmaCopyWordsAsynch(3, source, dest, count);
+#endif
 
-		return;
-	}
-
-	// Cannot use DMA as not working exclusively with VRAM
-	// Use for-loop instead
-	for (u32 i = 0; i < count; i++) {
-		*(dest + i) = *(source + i);
-	}
 }
 
 void woopsiDmaFill(u16 fill, u16* dest, u32 count) {
 
-	// Draw initial pixel
-	*dest = fill;
+#ifdef USING_SDL
 
-	// Stop copying if there are no more pixels to draw
-	if (count > 1) {
+    for (u32 i = 0; i < count; i++) {
+        *(dest + i) = fill;
+    }
 
-		u32 dsta = (u32)dest + 1;
+#else
 
-		// Precalculate boundaries of framebuffer VRAM
-		u32 bmp[2];
-		bmp[0] = 0x06000000;
-		bmp[1] = 0x06400000;
+    // Draw initial pixel
+    *dest = fill;
 
-		// Use DMA hardware if destination is within VRAM
-		if ((dsta >= bmp[0]) && (dsta < bmp[1])) {
+    // Stop copying if there are no more pixels to draw
+    if (count > 1) {
 
-			// libnds DMA functions work in bytes
-			count *= 2;
+        u32 dsta = (u32)dest + 1;
 
-			if((dsta|count) & 3)
-				dmaFillHalfWords(fill, dest, count);
-			else
-				dmaFillWords(fill, dest, count);
+        // Use DMA hardware if destination is within VRAM
+        if ((dsta >= MEM_VRAM_START) && (dsta < MEM_VRAM_END)) {
 
-			return;
-		}
-	}
+            // libnds DMA functions work in bytes
+            count *= 2;
 
-	// Cannot use DMA as not working exclusively with VRAM
-	// Use for-loop instead
-	for (u32 i = 0; i < count; i++) {
-		*(dest + i) = fill;
-	}
-}
+            while (dmaBusy(3)) {}
+
+            if ((dsta | count) & 3) {
+                dmaFillHalfWords(fill, dest, count);
+            } else {
+                dmaFillWords(fill, dest, count);
+            }
+
+            return;
+        }
+    }
+
+    // Cannot use DMA as not working exclusively with VRAM
+    for (u32 i = 0; i < count; i++) {
+        *(dest + i) = fill;
+    }
 
 #endif
+
+}
